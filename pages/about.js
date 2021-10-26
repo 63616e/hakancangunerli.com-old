97b1568b0a3a265854a6c05e3a92a39cb8f7fd10
hakancangunerli <hakancangunerli@gmail.com>
2021-10-26 14:31:59 -0400

    made a submenu as scrolled down

--- pages/about.js
+++ pages/about.js
@@ -9,15 +9,14 @@ export default function About() {
   return (
     <>
         
-        <span>heyo! </span>
         <div style={{}}>
           My name is Hakan Can Gunerli. Well, my government name is that, I go by John here.
         I'm passionate about the Human side of Computer Science, particularly working towards how humans interact with computers and computer-based systems like Robots and the internet. I'm also interested in the future of Artificial Intelligence, especially how it will impact the way we interact with computers.
         <br/>
         Besides all that, I'm a big on reading, linguistics, soccer - or football, depends where you're located - and buildings random things at midnight. also, big on memes. 
-      </div>
-      <br/>
-      <div style={{}}>
+      
+      <br/> 
+      
         I'm currently a student at the University of Georgia, studying Computer Science. I'm planning to graduate a semester early compared to my peers, but I'm not sure if I'll be able to do that. I'm currently a candidate for the Masters of Science in Computer Science combined program at the University of Georgia.
 
 </div>
